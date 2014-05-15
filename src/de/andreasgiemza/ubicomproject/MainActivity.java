3a69f45cb8babb6313478516068fea27fcfd847b
Andreas Giemza <andreas@giemza.net>
2014-05-15 19:59:48 +0200

    my location added

--- src/de/andreasgiemza/ubicomproject/MainActivity.java
+++ src/de/andreasgiemza/ubicomproject/MainActivity.java
@@ -3,11 +3,23 @@ package de.andreasgiemza.ubicomproject;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 public class MainActivity extends Activity {
+
+	private GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Create GoogleMap
+		googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+				R.id.map)).getMap();
+
+		// Show my location
+		googleMap.setMyLocationEnabled(true);
 	}
 }
