b59257555d48bf80bb08fad9fd3a4835bf80c1f7
berauser <benjamin.rauser@gmx.net>
2014-06-03 16:37:13 +0200

    adding NotificationService

---
+++ src/de/andreasgiemza/ubicomproject/services/NotificationService.java
@@ -0,0 +1,27 @@
+package de.andreasgiemza.ubicomproject.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NotificationService extends Service{
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+}
