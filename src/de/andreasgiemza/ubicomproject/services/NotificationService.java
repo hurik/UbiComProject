0187fcc1c04f334234e80be3bc0db1a4ec51de25
berauser <benjamin.rauser@gmx.net>
2014-06-03 17:13:17 +0200

    adding service to Manifest, implementing NotificationService, print Debug output

--- src/de/andreasgiemza/ubicomproject/services/NotificationService.java
+++ src/de/andreasgiemza/ubicomproject/services/NotificationService.java
@@ -3,48 +3,61 @@ package de.andreasgiemza.ubicomproject.services;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import de.andreasgiemza.ubicomproject.FtpServer;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+import de.andreasgiemza.ubicomproject.FtpServer;
 
-public class NotificationService extends Service{
+public class NotificationService extends Service {
 
-	private static final int PERIOD_MIN = 5;
+	private static final String TAG = "NotificationService";
+	
+	private static final int PERIOD_MIN = 2;
 	private static final int PERIOD_SEC = 0;
 	Timer timer = null;
 	FtpServer mFtpServer = null;
-	
-	
+
 	@Override
 	public void onCreate() {
+
+		Log.d(TAG,	"NotificationTask started");
 		
 		mFtpServer = new FtpServer(getApplicationContext());
-		
+
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
-			
+
 			@Override
 			public void run() {
 				// TODO check mftp == null
-				mFtpServer.read();				
+				if (mFtpServer == null) {
+					Log.e(TAG, "mFtpServer = null");
+					return;
+				}
+
+				Log.d(TAG, "mFtpServer.read()");
+				mFtpServer.read();
 			}
-		}, (long) 0, (long)((PERIOD_MIN * 60) + PERIOD_SEC) * 1000);
+		}, (long) 0, (long) ((PERIOD_MIN * 60) + PERIOD_SEC) * 1000);
+		
 		
 		super.onCreate();
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public void onDestroy() {
 
 		timer.cancel();
 		mFtpServer = null;
+
+		Log.d(TAG, "NoftificationService deleted");
 		
 		super.onDestroy();
 	}
