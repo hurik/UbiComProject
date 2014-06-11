5e656f0d0f5b5be2da09a20e49e92064ccaa6fad
Andreas Giemza <andreas@giemza.net>
2014-06-11 02:46:53 +0200

    Added show other positions

--- src/de/andreasgiemza/ubicomproject/dataserver/DataServer.java
+++ src/de/andreasgiemza/ubicomproject/dataserver/DataServer.java
@@ -1,10 +1,14 @@
 package de.andreasgiemza.ubicomproject.dataserver;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -17,6 +21,49 @@ public enum DataServer {
 	INSTANCE;
 
 	private final JSONParser jsonParser = new JSONParser();
+
+	public List<UbiCom_Pos> getPositions(Context applicationContext) {
+		final List<UbiCom_Pos> positions = new LinkedList<>();
+		final String number = getNumber(applicationContext);
+
+		if (checkInternetConnection(applicationContext)) {
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			// getting JSON string from URL
+			JSONObject json = jsonParser.makeHttpRequest(
+					"http://ucp.g8j.de/get_positions.php", "GET", params);
+
+			try {
+				if (json != null) {
+					// Checking for SUCCESS TAG
+					int success = json.getInt("success");
+
+					if (success == 1) {
+						// products found
+						// Getting Array of Products
+						JSONArray jsonPositions = json
+								.getJSONArray("positions");
+
+						// looping through All Products
+						for (int i = 0; i < jsonPositions.length(); i++) {
+							JSONObject c = jsonPositions.getJSONObject(i);
+
+							if (!number.equals(c.getString("number"))) {
+								positions.add(new UbiCom_Pos(c
+										.getString("number"), c
+										.getString("latitude"), c
+										.getString("longitude"), c
+										.getString("updated")));
+							}
+						}
+					}
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return positions;
+	}
 
 	public void updatePosition(Context applicationContext,
 			final Location location) {
@@ -57,5 +104,20 @@ public enum DataServer {
 		}
 
 		return false;
+	}
+
+	public class UbiCom_Pos {
+		public final String number;
+		public final String latitude;
+		public final String longitude;
+		public final String updated;
+
+		public UbiCom_Pos(String number, String latitude, String longitude,
+				String updated) {
+			this.number = number;
+			this.latitude = latitude;
+			this.longitude = longitude;
+			this.updated = updated;
+		}
 	}
 }
--- src/de/andreasgiemza/ubicomproject/services/NotificationService.java
+++ src/de/andreasgiemza/ubicomproject/services/NotificationService.java
@@ -1,73 +1,65 @@
 package de.andreasgiemza.ubicomproject.services;
 
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.util.Log;
+import android.support.v4.content.LocalBroadcastManager;
+import de.andreasgiemza.ubicomproject.dataserver.DataServer;
+import de.andreasgiemza.ubicomproject.dataserver.DataServer.UbiCom_Pos;
 
 public class NotificationService extends Service {
-
-	private static final String TAG = "NotificationService";
-
-	public static final String BROADCAST_ACTION = "FTP_POSITION_RECEIVED";
+	public static final String BROADCAST_ACTION = "NotificationService";
+	public static final String BROADCAST_NUMBER = "LocationNumber";
 	public static final String BROADCAST_LATITUDE = "LocationLatitude";
 	public static final String BROADCAST_LONGITUDE = "LocationLongitude";
+	public static final String BROADCAST_UPDATED = "LocationUpdated";
 
 	private static final int PERIOD_MIN = 0;
 	private static final int PERIOD_SEC = 20;
+
 	Timer timer = null;
 
 	// FtpServer mFtpServer = null;
 
 	@Override
 	public void onCreate() {
-
-		Log.d(TAG, "NotificationTask started");
-
-		// mFtpServer = new FtpServer(getApplicationContext());
+		super.onCreate();
 
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
 
 			@Override
 			public void run() {
-				// TODO check mftp == null
-				/*
-				 * if (mFtpServer == null) { Log.e(TAG, "mFtpServer = null");
-				 * return; }
-				 * 
-				 * Log.d(TAG, "mFtpServer.read()"); List<UbiCom_Pos> list =
-				 * mFtpServer.read();
-				 * 
-				 * if (list != null) for (UbiCom_Pos l : list) { Intent i = new
-				 * Intent(BROADCAST_ACTION); i.putExtra(BROADCAST_LATITUDE,
-				 * l.Latitude); i.putExtra(BROADCAST_LONGITUDE, l.Longtitude);
-				 * LocalBroadcastManager.getInstance(
-				 * getApplicationContext()).sendBroadcast(i); }
-				 */
+				List<UbiCom_Pos> list = DataServer.INSTANCE
+						.getPositions(getApplicationContext());
+
+				for (UbiCom_Pos l : list) {
+					Intent i = new Intent(BROADCAST_ACTION);
+					i.putExtra(BROADCAST_NUMBER, l.number);
+					i.putExtra(BROADCAST_LATITUDE,
+							Double.parseDouble(l.latitude));
+					i.putExtra(BROADCAST_LONGITUDE,
+							Double.parseDouble(l.longitude));
+					i.putExtra(BROADCAST_UPDATED, l.updated);
+					LocalBroadcastManager.getInstance(getApplicationContext())
+							.sendBroadcast(i);
+				}
 			}
 		}, (long) 0, (long) ((PERIOD_MIN * 60) + PERIOD_SEC) * 1000);
-
-		super.onCreate();
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void onDestroy() {
-
-		timer.cancel();
-		// mFtpServer = null;
-
-		Log.d(TAG, "NoftificationService deleted");
-
 		super.onDestroy();
+		timer.cancel();
 	}
 }
