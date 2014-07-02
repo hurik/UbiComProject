3e39e30134360f5cabccff400331987d7bf1be76
Andreas Giemza <andreas@giemza.net>
2014-07-02 17:57:25 +0200

    CalendarEvents refactort and GcmIntentService check if the user is busy befor notifying

--- src/de/andreasgiemza/ubicomproject/gcm/GcmIntentService.java
+++ src/de/andreasgiemza/ubicomproject/gcm/GcmIntentService.java
@@ -8,6 +8,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import de.andreasgiemza.ubicomproject.helpers.ApplicationData;
+import de.andreasgiemza.ubicomproject.helpers.CalendarEvents;
 
 public class GcmIntentService extends IntentService {
 
@@ -26,16 +27,20 @@ public class GcmIntentService extends IntentService {
 
 		if (!extras.isEmpty()) {
 			if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-				String[] data = extras.get("message").toString().split(";");
+				if (CalendarEvents.isBusy(getApplicationContext())) {
+					String[] data = extras.get("message").toString().split(";");
 
-				if (data.length == 3) {
-					// Save the new position
-					((ApplicationData)this.getApplication()).updatedPosition(data, getApplicationContext());
+					if (data.length == 3) {
+						// Save the new position
+						((ApplicationData) this.getApplication())
+								.updatedPosition(data, getApplicationContext());
 
-					// Inform the the main activity that there is a new position
-					Intent i = new Intent(BROADCAST_ACTION);
-					LocalBroadcastManager.getInstance(getApplicationContext())
-							.sendBroadcast(i);
+						// Inform the the main activity that there is a new
+						// position
+						Intent i = new Intent(BROADCAST_ACTION);
+						LocalBroadcastManager.getInstance(
+								getApplicationContext()).sendBroadcast(i);
+					}
 				}
 			}
 		}
--- src/de/andreasgiemza/ubicomproject/helpers/CalendarEvents.java
+++ src/de/andreasgiemza/ubicomproject/helpers/CalendarEvents.java
@@ -14,11 +14,79 @@ import android.provider.CalendarContract;
  * 
  */
 
-public enum CalendarEvents {
-	INSTANCE;
-	
+public final class CalendarEvents {
+
 	public final static int MAX_EVENT_LENGHT = 20; // In Hours
 	public final static int MS_PER_HOUR = 3600000;
+
+	public static MyCalenderEvent getNextEvent(Context context) {
+		return readCalendar(context, false);
+	}
+
+	public static MyCalenderEvent getCurrentEvent(Context context) {
+		return readCalendar(context, true);
+	}
+
+	public static boolean isBusy(Context context) {
+		return (readCalendar(context, true) != null);
+
+	}
+
+	/*
+	 * current == true -> get current Event current == false -> getNextEvent
+	 */
+	private static MyCalenderEvent readCalendar(Context context, boolean current) {
+
+		long time = Calendar.getInstance().getTimeInMillis();
+
+		ContentResolver contentResolver = context.getContentResolver();
+
+		// Get Title, Start and End-Time, Description
+		String[] projection = new String[] { CalendarContract.Events.TITLE,
+				CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND,
+				CalendarContract.Events.DESCRIPTION };
+
+		String selection = null;
+		if (current) {
+
+			selection = "(" + CalendarContract.Events.DTSTART + " < " + time
+					+ " and " + time + " < " + CalendarContract.Events.DTEND
+					+ " )";
+		} else {
+			selection = "( " + CalendarContract.Events.DTSTART + " >" + time
+					+ " )";
+		}
+
+		Cursor cursor = contentResolver.query(
+				CalendarContract.Events.CONTENT_URI, projection, selection,
+				null, CalendarContract.Events.DTSTART + " ASC");
+		if (!cursor.moveToFirst())
+			return null; // Keine Daten -> return
+
+		long start_ms = cursor.getLong(1);
+		long end_ms = cursor.getLong(2);
+
+		// delete all Events with more than MAX_EVENT_LENGHT Hours
+		while (true) {
+			if ((end_ms - start_ms) >= MAX_EVENT_LENGHT * MS_PER_HOUR) { // next
+																			// event?
+				if (!cursor.moveToNext())
+					return null;
+			} else {
+				break;
+			}
+		}
+
+		Date start = new Date(start_ms);
+		Date end = new Date(end_ms);
+
+		MyCalenderEvent event = new MyCalenderEvent(cursor.getString(0), start,
+				end, cursor.getString(3));
+
+		cursor.close();
+
+		return event;
+	}
 
 	public static class MyCalenderEvent {
 		private String title;
@@ -26,7 +94,8 @@ public enum CalendarEvents {
 		private Date end;
 		private String description;
 
-		public MyCalenderEvent(String title, Date start, Date end, String description) {
+		public MyCalenderEvent(String title, Date start, Date end,
+				String description) {
 			this.title = title;
 			this.start = start;
 			this.end = end;
@@ -64,74 +133,5 @@ public enum CalendarEvents {
 		public void setDescription(String description) {
 			this.description = description;
 		}
-	}
-
-	public MyCalenderEvent getNextEvent(Context context) {
-		return readCalendar(context, false);
-	}
-
-	public MyCalenderEvent getCurrentEvent(Context context) {
-		return readCalendar(context, true);
-	}
-
-	public boolean isBusy(Context context) {
-		return (readCalendar(context, true) != null);
-
-	}
-
-	/*
-	 * current == true -> get current Event current == false -> getNextEvent
-	 */
-	private MyCalenderEvent readCalendar(Context context, boolean current) {
-
-		long time = Calendar.getInstance().getTimeInMillis();
-
-		ContentResolver contentResolver = context.getContentResolver();
-
-		// Get Title, Start and End-Time, Description
-		String[] projection = new String[] { CalendarContract.Events.TITLE,
-				CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND,
-				CalendarContract.Events.DESCRIPTION };
-
-		String selection = null;
-		if (current) {
-
-			selection = "(" + CalendarContract.Events.DTSTART + " < " + time
-					+ " and " + time + " < " + CalendarContract.Events.DTEND
-					+ " )";
-		} else {
-			selection = "( " + CalendarContract.Events.DTSTART + " >" + time
-					+ " )";
-		}
-
-		Cursor cursor = contentResolver.query(
-				CalendarContract.Events.CONTENT_URI, projection, selection,
-				null, CalendarContract.Events.DTSTART + " ASC");
-		if (!cursor.moveToFirst())
-			return null; // Keine Daten -> return
-
-		long start_ms = cursor.getLong(1);
-		long end_ms = cursor.getLong(2);
-
-		// delete all Events with more than MAX_EVENT_LENGHT Hours
-		while (true) {
-			if ((end_ms - start_ms) >= MAX_EVENT_LENGHT * MS_PER_HOUR) { // next event?
-				if (!cursor.moveToNext())
-					return null;
-			} else {
-				break;
-			}
-		}
-
-		Date start = new Date(start_ms);
-		Date end = new Date(end_ms);
-
-		MyCalenderEvent event = new MyCalenderEvent(cursor.getString(0), start,
-				end, cursor.getString(3));
-
-		cursor.close();
-
-		return event;
-
 	}
 }
